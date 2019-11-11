c1b302f012c02252ce6c56fb260b732635e24de6
Sean <DevRain2500@users.noreply.github.com>
2019-11-11 00:45:41 -0500

    Update __resource.lua

--- airplanes/__resource.lua
+++ airplanes/__resource.lua
@@ -1,5 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 dependency 'NativeUI'
+dependency 'pNotify'
+dependency 'tokovoip_script'
 description "Airplane rental script made by DevoutRain2500"
 
 files {
