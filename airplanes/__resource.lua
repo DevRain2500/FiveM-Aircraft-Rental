418b5d3661824acf10488284a87e7d6ac965a8d0
Sean <DevRain2500@users.noreply.github.com>
2019-11-11 02:48:29 -0500

    Update __resource.lua

--- airplanes/__resource.lua
+++ airplanes/__resource.lua
@@ -1,22 +1,9 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
 dependency 'NativeUI'
 dependency 'pNotify'
-dependency 'tokovoip_script'
+
 description "Airplane rental script made by DevoutRain2500"
-
-files {
-	'vehiclelayouts.meta',
-	'scamp_vehiclelayouts.meta',
-	'vehicles.meta',
-	'carvariations.meta',
-	'handling.meta',
-}
-
-data_file 'VEHICLE_LAYOUTS_FILE' 'scamp_vehiclelayouts.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
-data_file 'HANDLING_FILE' 'handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 
 client_script '@NativeUI/NativeUI.lua'
 client_script "c_airplanes.lua"
