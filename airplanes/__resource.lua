fe4e867c37661466090f04bcfe8c029e9da5e9f9
Sean <DevRain2500@users.noreply.github.com>
2019-11-11 00:45:12 -0500

    Update __resource.lua

--- airplanes/__resource.lua
+++ airplanes/__resource.lua
@@ -1,4 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+dependency 'NativeUI'
+description "Airplane rental script made by DevoutRain2500"
 
 files {
 	'vehiclelayouts.meta',
@@ -14,5 +16,5 @@ data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 
+client_script '@NativeUI/NativeUI.lua'
 client_script "c_airplanes.lua"
-client_script "@utils/c_main.lua"
